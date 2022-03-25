efb1d8870cca7ebebdcd12e3a4fea81521f9020b
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-03-25 23:21:54 +0500

    реализован класс для управления экранами

---
+++ core/src/com/mygdx/game/ScreenManager.java
@@ -0,0 +1,42 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ScreenManager {
+    public enum ScreenType {
+        MENU, GAME
+    }
+
+    private static ScreenManager ourInstance = new ScreenManager();
+
+    public static ScreenManager getInstance() {
+        return ourInstance;
+    }
+
+    private ScreenManager() {
+    }
+
+    private Game game;
+    private GameScreen gameScreen;
+
+    public void init(Game game, SpriteBatch spriteBatch) {
+        this.game = game;
+        this.gameScreen = new GameScreen(spriteBatch);
+    }
+
+    public void setScreen(ScreenType screenType) {
+        Screen currentScreen = game.getScreen();
+        switch (screenType) {
+            case GAME:
+                game.setScreen(gameScreen);
+                break;
+        }
+        if (currentScreen != null) {
+            currentScreen.dispose();
+        }
+    }
+}
+
+
