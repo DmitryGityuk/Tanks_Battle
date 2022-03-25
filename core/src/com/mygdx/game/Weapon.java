316f01f15661c00b090a561eac1668923d217c5b
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-03-25 23:33:27 +0500

    реализован класс для работы со стрельбой танка

--- core/src/com/mygdx/game/Weapon.java
+++ core/src/com/mygdx/game/Weapon.java
@@ -7,6 +7,9 @@ public class Weapon {
     private float fireTimer;
     private float firePeriod;
     private int damage;
+    private float radius;
+    private float projectileSpeed;
+    private float projectileLifeTime;
     private TextureRegion texture;
 
     public Weapon(TextureAtlas atlas) {
@@ -14,6 +17,9 @@ public class Weapon {
         this.fireTimer = 0.0f;
         this.firePeriod = 0.4f;
         this.damage = 1;
+        this.radius = 300.0f;
+        this.projectileSpeed = 320.0f;
+        this.projectileLifeTime = this.radius / this.projectileSpeed;
     }
 
     public float getFireTimer() {
@@ -30,5 +36,17 @@ public class Weapon {
 
     public TextureRegion getTexture() {
         return texture;
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public float getProjectileSpeed() {
+        return projectileSpeed;
+    }
+
+    public float getProjectileLifeTime() {
+        return projectileLifeTime;
     }
 }
