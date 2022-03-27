f2aeabd5e998ec8d3e5fa0ecc5c7d0cfb6b366a2
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-03-27 20:14:23 +0500

    изменена отрисовка, добавлено управление для второго игрока

--- core/src/com/mygdx/game/units/PlayerTank.java
+++ core/src/com/mygdx/game/units/PlayerTank.java
@@ -10,16 +10,23 @@ import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameScreen;
 import com.mygdx.game.Weapon;
 import com.mygdx.game.utils.Direction;
+import com.mygdx.game.utils.KeysControl;
 import com.mygdx.game.utils.TankOwner;
+import com.mygdx.game.utils.Utils;
 
 public class PlayerTank extends Tank {
-    int lives;
+    KeysControl keysControl;
+    StringBuilder tmpString;
+    int index;
     int score;
+    int lives;
 
-    public PlayerTank(GameScreen game, TextureAtlas atlas) {
+    public PlayerTank(int index, GameScreen game, KeysControl keysControl, TextureAtlas atlas) {
         super(game);
-        this.weapon = new Weapon(atlas);
+        this.index = index;
         this.ownerType = TankOwner.PLAYER;
+        this.keysControl = keysControl;
+        this.weapon = new Weapon(atlas);
         this.texture = atlas.findRegion("playerTankBase");
         this.textureHP = atlas.findRegion("bar");
         this.position = new Vector2(100.0f, 100.0f);
@@ -30,39 +37,51 @@ public class PlayerTank extends Tank {
         this.hp = this.hpMax;
         this.circle = new Circle(position.x, position.y, (width + height) / 2);
         this.lives = 5;
+        this.tmpString = new StringBuilder();
     }
 
-    /**
-     * Метод реализует движение танка
-     */
     public void checkMovement(float dt) {
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+        if (Gdx.input.isKeyPressed(keysControl.getLeft())) {
             move(Direction.LEFT, dt);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+        } else if (Gdx.input.isKeyPressed(keysControl.getRight())) {
             move(Direction.RIGHT, dt);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+        } else if (Gdx.input.isKeyPressed(keysControl.getUp())) {
             move(Direction.UP, dt);
-        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+        } else if (Gdx.input.isKeyPressed(keysControl.getDown())) {
             move(Direction.DOWN, dt);
         }
     }
 
-    /**
-     * Метод реализует обновление положения танка
-     */
     public void update(float dt) {
         checkMovement(dt);
-        float mx = Gdx.input.getX();
-        float my = Gdx.graphics.getHeight() - Gdx.input.getY();
-        rotateTurretToPoint(mx, my, dt);
-        if (Gdx.input.isTouched()) {
-            fire();
+        if (keysControl.getTargeting() == KeysControl.Targeting.MOUSE) {
+            rotateTurretToPoint(gameScreen.getMousePosition().x, gameScreen.getMousePosition().y, dt);
+            if (Gdx.input.isTouched()) {
+                fire();
+            }
+        } else {
+            if (Gdx.input.isKeyPressed(keysControl.getRotateTurretLeft())) {
+                turretAngle = Utils.makeRotation(turretAngle, turretAngle + 90.0f, 270.0f, dt);
+                turretAngle = Utils.angleToFromNegPiToPosPi(turretAngle);
+            }
+            if (Gdx.input.isKeyPressed(keysControl.getRotateTurretRight())) {
+                turretAngle = Utils.makeRotation(turretAngle, turretAngle - 90.0f, 270.0f, dt);
+                turretAngle = Utils.angleToFromNegPiToPosPi(turretAngle);
+            }
+            if (Gdx.input.isKeyPressed(keysControl.getFire())) {
+                fire();
+            }
+
         }
         super.update(dt);
     }
 
-    public void renderHUD(SpriteBatch batch, BitmapFont font) {
-        font.draw(batch, "Score: " + score + "\nLives: " + lives, 10, 700);
+    public void renderHUD(SpriteBatch batch, BitmapFont font24) {
+        tmpString.setLength(0);
+        tmpString.append("Player: ").append(index);
+        tmpString.append("\nScore: ").append(score);
+        tmpString.append("\nLives: ").append(lives);
+        font24.draw(batch, tmpString, 20 + (index - 1) * 200, 700);
     }
 
     public void addScore(int amount) {
