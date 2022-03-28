914f06babde8d3eb1296df3d6b7c99fdec83b709
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-03-28 22:02:38 +0500

    изменён экран старта игры

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -8,12 +8,11 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MyGdxGame extends Game {
     private SpriteBatch batch;
 
-
     @Override
     public void create() {
         batch = new SpriteBatch();
         ScreenManager.getInstance().init(this, batch);
-        ScreenManager.getInstance().setScreen(ScreenManager.ScreenType.GAME);
+        ScreenManager.getInstance().setScreen(ScreenManager.ScreenType.MENU);
     }
 
     @Override
