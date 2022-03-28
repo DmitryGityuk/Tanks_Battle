566e2e07cb935f1f033c447842aa3d76e5f704e3
Dmitry Gityuk <Dmitrygityuk@gmail.com>
2022-03-28 21:39:57 +0500

    добавлен экран выбора режима игры

--- core/src/com/mygdx/game/ScreenManager.java
+++ core/src/com/mygdx/game/ScreenManager.java
@@ -1,12 +1,14 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.utils.GameType;
 
 public class ScreenManager {
     public enum ScreenType {
@@ -17,6 +19,7 @@ public class ScreenManager {
     public final static int WORLD_HEIGHT = 720;
     private Game game;
     private GameScreen gameScreen;
+    private MenuScreen menuScreen;
     private Viewport viewport;
     private Camera camera;
     private static ScreenManager ourInstance = new ScreenManager();
@@ -43,12 +46,18 @@ public class ScreenManager {
         this.camera.update();
         this.viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
         this.gameScreen = new GameScreen(spriteBatch);
+        this.menuScreen = new MenuScreen(spriteBatch);
     }
 
-    public void setScreen(ScreenType screenType) {
+    public void setScreen(ScreenType screenType, Object... args) {
+        Gdx.input.setCursorCatched(false);
         Screen currentScreen = game.getScreen();
         switch (screenType) {
+            case MENU:
+                game.setScreen(menuScreen);
+                break;
             case GAME:
+                gameScreen.setGameType((GameType) args[0]);
                 game.setScreen(gameScreen);
                 break;
         }
